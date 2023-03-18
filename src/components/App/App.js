04c044f667cbed8e50aceee654d1b81c496ddae1
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-03-18 17:00:52 +0200

    fix little mistakes

---
+++ src/components/App/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Container, Notification } from './App.styled';
+import Section from 'components/Section';
+import FeedbackOptions from 'components/FeedbackOptions';
+import Statistics from 'components/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = e => {
+    const { value } = e.target;
+    this.setState(prevState => ({
+      [value]: prevState[value] + 1,
+    }));
+  };
+
+  countTotalFeedback = () =>
+    Object.values(this.state).reduce((total, value) => total + value);
+
+  countPositiveFeedbackPercentage = () =>
+    Math.round((this.state.good / this.countTotalFeedback()) * 100);
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positive = this.countPositiveFeedbackPercentage();
+    return (
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleClick}
+          />
+        </Section>
+        <Section title="Statistics">
+          {good || neutral || bad ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positive}
+            />
+          ) : (
+            <Notification>There is no feedback</Notification>
+          )}
+          ;
+        </Section>
+      </Container>
+    );
+  }
+}
+
+// export default App;
